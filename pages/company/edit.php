9f29a7973bb89b56f1ca28a1b59b310127d95e1d
VuongTuanAnh165 <92902120+VuongTuanAnh165@users.noreply.github.com>
2021-10-27 15:16:45 +0700

    updateta123lan2

--- pages/company/edit.php
+++ pages/company/edit.php
@@ -1,20 +1,17 @@
 <?php
-require_once(__DIR__ . '/../../layout/header.php');
-?>
-
-<?php
 $open = "company";
 require_once(__DIR__ . '/../../autoload/autoload.php');
 
 $id = intval(getInput('id'));
-$sql = "SELECT * FROM company WhERE company_id=$id";
-$company = $db->fetchcheck($sql);
-if (empty($company)) {
+$sql_check = "SELECT * FROM company WhERE company_id=$id";
+$company_check = $db->fetchcheck($sql_check);
+if (empty($company_check)) {
     $_SESSION['error'] = "Dữ liệu không tồn tại";
     redirectAdmin($open);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors_img = "";
     $file_name = $_FILES['company_logo']['name'];
     $file_size = $_FILES['company_logo']['size'];
     $file_tmp = $_FILES['company_logo']['tmp_name'];
@@ -22,19 +19,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $file_parts = explode('.', $_FILES['company_logo']['name']);
     $file_ext = strtolower(end($file_parts));
     $expensions = array("jpeg", "jpg", "png");
+
     $company_logo = $_FILES['company_logo']['name'];
     $target = "photo/" . basename($company_logo);
     $data =
         [
             "company_name" => postInput('company_name'),
+            "company_logo" => $company_logo,
             "company_email" => postInput('company_email'),
             "company_address" => postInput('company_address'),
             "company_mobile" => postInput('company_mobile'),
             "company_phone" => postInput('company_phone'),
-            "company_logo" => $company_logo,
-            "company_description" => postInput('company_description')
+            "company_description" => postInput('company_description'),
+            "company_content" => postInput('company_content')
         ];
-
     if (postInput('company_name') == '') {
         echo "<script>alert('Mời bạn nhập đầy đủ tên công ty');</script>";
     } else {
@@ -69,6 +67,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+<?php
+require_once(__DIR__ . '/../../layout/header.php');
+?>
 
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -76,12 +77,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Thay đổi thông tin công ty</h1>
+                    <h1>Thay đổi Dự án</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="../../index.php">Trang chủ</a></li>
-                        <li class="breadcrumb-item"><a href="./index.php">Thông tin công ty</a></li>
+                        <li class="breadcrumb-item"><a href="./index.php">Danh sách Dự án</a></li>
                         <li class="breadcrumb-item active">Sửa</li>
                     </ol>
                 </div>
@@ -99,8 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-lable">Tên công ty</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên công ty" name='company_name' value="<?php echo $company['company_name'] ?>">
-
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên Công ty" name='company_name' value="<?php echo $company_check['company_name'] ?>">
                             </div>
                         </div>
 
@@ -108,46 +108,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <label for="exampleFormControlFile1">Logo</label>
                             <div class="col-sm-8">
                                 <input type="file" class='form-control-file' id="exampleFormControlFile1" name='company_logo' onchange="preview_thumbail_logo(this);">
-                                <img id="logo" width="100px" src="./photo/<?php echo $company['company_logo'] ?>" alt="<?php echo $company['company_logo'] ?>">
+                                <img width="100px" id="logo" src="./photo/<?php echo $company_check['company_logo'] ?>" alt="your image">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-lable">Email</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên miền" name='company_email' value="<?php echo $company['company_email'] ?>">
-
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-lable">Hostline</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên miền" name='company_phone' value="<?php echo $company['company_phone'] ?>">
-                
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-lable">Số điện thoại</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên miền" name='company_mobile' value="<?php echo $company['company_mobile'] ?>">
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="Email" name='company_email' value="<?php echo $company_check['company_email'] ?>">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-lable">Địa chỉ</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Tên miền" name='company_address' value="<?php echo $company['company_address'] ?>">
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="Địa chỉ" name='company_address' value="<?php echo $company_check['company_address'] ?>">
+                            </div>
+                        </div>
 
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-lable">Số điện thoại</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="Số điện thoại" name='company_mobile' value="<?php echo $company_check['company_mobile'] ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-lable">Hotline</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="hotline" name='company_phone' value="<?php echo $company_check['company_phone'] ?>">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-lable">Mô tả</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="inputEmail3" placeholder="Mô tả" name='company_description' value="<?php echo $company['company_description'] ?>">
-
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="Mô tả" name='company_description' value="<?php echo $company_check['company_description'] ?>">
                             </div>
                         </div>
 
@@ -155,7 +151,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <label for="inputEmail3" class="col-sm-2 control-lable">Nội dung</label>
                             <div class="col-sm-8">
                                 <textarea class="form-control" rows="3" id="summernote" name='company_content'>
-                                    <?php echo $company['company_content'] ?>
+                                    <?php echo $company_check['company_content'] ?>
                                 </textarea>
                             </div>
                         </div>
